741ade66128c13e4467e2c11e3ba81b4edf4ed2b
Mohamed Ibrahim <zhaaficimaxamed12@gmail.com>
2026-09-17 07:30:06 +0300

    wa exercise 1

---
+++ exercise react/src/App.jsx
@@ -0,0 +1,14 @@
+import Usercard from "./Username";
+
+const App = () => {
+  return (
+    <div>
+      <Usercard />
+    </div>
+  );
+};
+<body>
+  <h1>john</h1>
+</body>;
+
+export default App;
---
+++ exercise1/Username.jsx
@@ -0,0 +1,10 @@
+const Username = () => {
+  return (
+    <div>
+      <h1>username: maxamed</h1>
+      <p>email: cali</p>
+    </div>
+  );
+};
+
+export default Username;
